38c6b1cddf2a99d760fea28abea71490fdf24578
Akhter Alamin <akhter.amin@ipay.com.bd>
2017-10-25 10:24:42 +0600

    IPAY-1485:date time teplate tag fixed

--- admin-auth/templatetags/datetime.py
+++ admin-auth/templatetags/datetime.py
@@ -7,7 +7,6 @@ register = template.Library()
 def get_date_time(value, arg):
     # timestamp = "2008-09-26T01:51:42.000Z"
     ts = time.strptime(value[:19], "%Y-%m-%dT%H:%M:%S")
-    time.strftime("%m/%d/%Y %H:%M:%S", ts)
-    print(value)
-    print(time.strftime("%m/%d/%Y %H:%M", ts))
-    return time.strftime("%m/%d/%Y %H:%M", ts)
+    time.strftime("%d/%m/%Y %H:%M:%S", ts)
+    
+    return time.strftime("%d/%m/%Y %H:%M", ts)
