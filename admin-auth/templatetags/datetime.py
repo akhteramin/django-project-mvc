f5a075853db0490ec2a3867ef17ac43e675902b3
Akhter Alamin <akhter.amin@ipay.com.bd>
2017-10-25 09:54:23 +0600

    IPAY-1485:date time problem in activity list has been fixed

--- admin-auth/templatetags/datetime.py
+++ admin-auth/templatetags/datetime.py
@@ -6,8 +6,8 @@ register = template.Library()
 @register.filter
 def get_date_time(value, arg):
     # timestamp = "2008-09-26T01:51:42.000Z"
-    ts = time.strptime(value[:19], "%Y-%m-%dT%I:%M:%S")
-    time.strftime("%m/%d/%Y %I:%M:%S", ts)
+    ts = time.strptime(value[:19], "%Y-%m-%dT%H:%M:%S")
+    time.strftime("%m/%d/%Y %H:%M:%S", ts)
     print(value)
-    print(time.strftime("%m/%d/%Y %I:%M", ts))
-    return time.strftime("%m/%d/%Y %I:%M", ts)
+    print(time.strftime("%m/%d/%Y %H:%M", ts))
+    return time.strftime("%m/%d/%Y %H:%M", ts)
