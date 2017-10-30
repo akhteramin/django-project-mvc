0cedc89f0a733f89ed5fefbabba4924c5400df13
Akhter Alamin <akhter.amin@ipay.com.bd>
2017-10-30 11:18:45 +0600

    IPAY-1485: create button not disabled

--- admin-auth/static/admin-auth/template_js/create_service.js
+++ admin-auth/static/admin-auth/template_js/create_service.js
@@ -1,14 +1,14 @@
-$('#appID, #serviceID, #serviceCategory, #description').bind('keyup', function() {
-    if(allFilled())
-        $('#create_service').removeAttr('disabled');
-    else
-        $('#create_service').prop("disabled", true);
-});
-
-function allFilled() {
-    var filled = true;
-    $('body input').each(function() {
-        if($(this).val() == '') filled = false;
-    });
-    return filled;
-}
+//$('#appID, #serviceID, #serviceCategory, #description').bind('keyup', function() {
+//    if(allFilled())
+//        $('#create_service').removeAttr('disabled');
+//    else
+//        $('#create_service').prop("disabled", true);
+//});
+//
+//function allFilled() {
+//    var filled = true;
+//    $('body input').each(function() {
+//        if($(this).val() == '') filled = false;
+//    });
+//    return filled;
+//}
