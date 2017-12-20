6d74d24a449457d3853420fd39c293fc579bc50b
Akhter Alamin <akhter.amin@ipay.com.bd>
2017-12-20 11:39:24 +0600

    IPAY-2371: allowed host all

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = 'z)!9&2**@6ev!=l0cd=@1#qobpl0eiujui1sn6&&8xfl^31d2('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.10.10.199','localhost','127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
